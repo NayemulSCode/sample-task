a1e40dd34c4c454282a8c7208c77cd88a520c7ca
hammadcse <hammadrahman162@gmail.com>
2023-05-29 01:39:19 +0600

    bottom header and banner design start

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 import { Swiper, SwiperSlide, Navigation, Pagination, EffectFade } from '../ui/slider'
 const banners = [
     {
-        title: 'Shop Computer & experience',
-        description: `You cannot inspect quality into the product; it is already there.
-                    I am not a product of my circumstances.I am a product of my decisions.`,
+        // title: 'Shop Computer & experience',
+        // description: `You cannot inspect quality into the product; it is already there.
+        // I am not a product of my circumstances. I am a product of my decisions.`,
         image: bannerImage1
         }
 ]
@@ -38,16 +38,18 @@ const Banner:FC = () => {
                           banners.map((banner: any, index: number) => (
                               <SwiperSlide key={index}>
                                   <div className='relative'>
-                                      <div className={`lg:w-[50%] w-full  z-[99] lg:space-s-8 px-4 md:!pl-[65px] md:px-6 lg:px-8 `}>
+                                      <div className={`lg:w-[50%] w-full  z-[99] lg:space-s-8 px-4 md:!pl-[65px] md:px-6 lg:px-8 swiper-catch`}>
                                           <div className="slider__content md:block hidden">
-                                              <h2 className="lg:text-[50px] md:text-[45px] text-[20px] font-bold md:leading-[60px] leading-[25px] lg:mb-[35px] md:mb-[25px] mb-[8px] text-black uppercase slider__maintitle">
-                                                  {banner?.title}
-                                              </h2>
+                                              <h1 className="lg:text-[57px] md:text-[45px] text-[20px] font-normal md:leading-[53px] leading-[25px] mb-[8px] text-[#0AAEB9] capitalize slider__maintitle">
+                                                    <span className='text-[#000]'>Shop</span> Computer & experience
+                                              </h1>
+                                              <p className='mb-2'>You cannot inspect quality into the product; it is already there. <br />
+                                                I am not a product of my circumstances. I am a product of my decisions.</p>
                                                 <a
-                                                    className="md:w-[168px] w-[100px] cursor-pointer tracking-widest flex items-center justify-center bg-primary text-black hover:bg-dark lg:text-[16px] text-[14px] font-bold lg:p-[12px] md:p-[8px] p-[6px]"
+                                                    className="md:w-[168px] w-[100px] cursor-pointer tracking-widest flex items-center justify-center bg-[#14B1F0] text-[#fff] hover:bg-dark lg:text-[16px] text-[14px] font-bold lg:p-[12px] md:p-[8px] p-[6px]"
                                                 //   onClick={() => }
                                                 >
-                                                    SHOP NOW
+                                                    View More
                                                 </a>
                                           </div>
                                       </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,8 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FaSearch, FaHeadphones, FaUser } from 'react-icons/fa'
+import { FaSearch, FaHeadphones, FaUser, FaBars, FaFacebookF, FaTwitter, FaLinkedin, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { FiShoppingCart } from 'react-icons/fi'
 import Logo from '../public/image/logo.png'
@@ -9,69 +10,121 @@ import Logo from '../public/image/logo.png'
 const Header = () => {
   return (
     <div className=''>
-          <div className="max-w-[1640px] w-full mx-auto lg:px-8 md:px-6 px-4 flex items-center justify-between bg-[#03484D]">
-        <div className='flex items-center'>
-            <Link href="#">
-                <Image
-                    src={Logo}
-                    width={150}
-                    title='winstore_logo'
-                    alt='winstore_logo'
-                    className='mr-[30px]'
-                />
-            </Link>
-            <div className='flex h-[40px] bg-[#fff] py-[5px] px-[18px] rounded-[10px]'>
-                <select className='select_cat h-full border-0 outline-0 !shadow-none text-[#ABA3A3]' name="categories" id="categories">
-                    <option defaultValue="All Categories" value="all categories">All Categories</option>
-                    <option value="Electronics">Electronics</option>
-                    <option value="Fashion">Fashion</option>
-                </select>
-                <div className="relative srcch_wrap">
-                          <input className='md:w-[265px] h-full text-[#ABA3A3] pl-[20px] border-0 outline-0 !shadow-none' type="text" placeholder='Search for products' />
-                    <div className='w-[50px] h-[40px] flex items-center justify-center top-[-5px] rounded-tr-[10px] rounded-br-[10px] bg-[#B6B6B6] cursor-pointer absolute right-[-18px]'>
-                        <FaSearch className='text-[#fff] text-[22px]' />
+        <div className="max-w-[1640px] w-full mx-auto lg:px-8 md:px-6 px-4 py-[7px] flex items-center justify-between bg-[#03484D]">
+            <div className='flex items-center'>
+                <Link href="#">
+                    <Image
+                        src={Logo}
+                        width={150}
+                        title='winstore_logo'
+                        alt='winstore_logo'
+                        className='mr-[30px]'
+                    />
+                </Link>
+                <div className='flex h-[40px] bg-[#fff] py-[5px] px-[18px] rounded-[10px]'>
+                    <select className='select_cat h-full border-0 outline-0 !shadow-none text-[#ABA3A3]' name="categories" id="categories">
+                        <option defaultValue="All Categories" value="all categories">All Categories</option>
+                        <option value="Electronics">Electronics</option>
+                        <option value="Fashion">Fashion</option>
+                    </select>
+                    <div className="relative srcch_wrap">
+                            <input className='md:w-[320px] h-full text-[#ABA3A3] pl-[20px] border-0 outline-0 !shadow-none' type="text" placeholder='Search for products' />
+                        <div className='w-[50px] h-[40px] flex items-center justify-center top-[-5px] rounded-tr-[10px] rounded-br-[10px] bg-[#B6B6B6] cursor-pointer absolute right-[-18px]'>
+                            <FaSearch className='text-[#fff] text-[22px]' />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className='flex'>
-            <ul className='flex items-center text-[#fff]'>
-                <li className='mr-[15px]'>
-                    <div>
-                        <span className='block text-[13px]'>Call Us Now</span>
-                        <Link href="tel: 011 5827918">
-                            <div className="flex">
-                                <FaHeadphones className='mr-[5px]' />
-                                <span>+011 5827918</span>
+            <div className='flex'>
+                <ul className='flex items-center text-[#fff]'>
+                    <li className='mr-[20px]'>
+                        <div>
+                            <span className='block text-[13px]'>Call Us Now</span>
+                            <Link href="tel: 011 5827918">
+                                <div className="flex">
+                                    <FaHeadphones className='mr-[5px]' />
+                                    <span>+011 5827918</span>
+                                </div>
+                            </Link>
+                            <Link href="#" className='block text-[13px]'>Sign In</Link>
+                        </div>
+                    </li>
+                    <li className='mr-[20px]'>
+                        <Link href="#">
+                            <FaUser className='text-[22px]' />
+                        </Link>
+                    </li>
+                    <li className='mr-[20px]'>
+                        <Link href="#">
+                            <AiOutlineHeart className='text-[22px]' />
+                        </Link>
+                    </li>
+                    <li className=''>
+                        <Link href="#">
+                            <div className='flex'>
+                            <div className="relative mr-[3px]">
+                                    <FiShoppingCart className='text-[22px]' />
+                                    <span className='absolute top-[-17px] right-[5px] text-[#FDDE3B]'>3</span>
+                            </div>
+                            <span>Cart</span>
                             </div>
                         </Link>
-                        <Link href="#" className='block text-[13px]'>Sign In</Link>
-                    </div>
-                </li>
-                <li className='mr-[15px]'>
-                    <Link href="#">
-                        <FaUser className='text-[22px]' />
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div className="max-w-[1640px] w-full mx-auto lg:px-8 md:px-6 px-4 py-[10px] flex items-center justify-between bg-[#0E3B3E]">
+            <ul className='flex items-center text-[#fff]'>
+                <li className='mr-[20px]'>
+                    <Link href="#" className='flex items-center text-[15px]'>
+                        <FaBars className='mr-[7px] mb-[2px]' />
+                        <span>Brouse By Category</span>
                     </Link>
                 </li>
-                <li className='mr-[15px]'>
+                <li className='mr-[20px]'>
+                    <Link href="#" className='text-[15px]'>
+                        Home
+                    </Link>
+                </li>
+                <li className='mr-[20px]'>
+                    <Link href="#" className='text-[15px]'>
+                        Easy Monthly Installments
+                    </Link>
+                </li>
+                <li className='mr-[20px]'>
+                    <Link href="#" className='text-[15px]'>
+                        Shop by Brands
+                    </Link>
+                </li>
+                <li className='mr-[20px]'>
+                    <Link href="#" className='text-[15px]'>
+                        Become a Vendor
+                    </Link>
+                </li>
+            </ul>
+            <ul className="flex items-center text-[#fff]">
+                <li className='mr-[20px]'>
                     <Link href="#">
-                        <AiOutlineHeart />
+                        <FaFacebookF className='text-[22px]' />
+                    </Link>
+                </li>
+                <li className='mr-[20px]'>
+                    <Link href="#">
+                        <FaTwitter className='text-[22px]' />
+                    </Link>
+                </li>
+                <li className='mr-[20px]'>
+                    <Link href="#">
+                        <FaLinkedinIn className='text-[22px]' />
                     </Link>
                 </li>
                 <li className=''>
                     <Link href="#">
-                        <div className='flex'>
-                           <div className="relative">
-                                <FiShoppingCart className='text-[22px]' />
-                                <span className='absolute top-[-15px]'>3</span>
-                           </div>
-                           <span>Cart</span>
-                        </div>
+                        <FaInstagram className='text-[22px]' />
                     </Link>
                 </li>
             </ul>
         </div>
-    </div>
     </div>
   )
 }
