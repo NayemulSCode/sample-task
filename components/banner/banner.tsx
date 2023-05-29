73a2a7746d296edf2cc93e2f892b7180b7b391c6
hammadcse <hammadrahman162@gmail.com>
2023-05-29 23:26:28 +0600

    banner design done

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { FC } from 'react'
-import bannerImage1 from '../../public/image/banner-image.jpg'
+import bannerImage1 from '../../public/image/banner1.png'
+import bannerImage2 from '../../public/image/banner2.jpg'
+import bannerImage3 from '../../public/image/banner3.jpg'
 import Image from 'next/image'
 import { Swiper, SwiperSlide, Navigation, Pagination, EffectFade } from '../ui/slider'
 const banners = [
@@ -9,7 +11,13 @@ const banners = [
         // description: `You cannot inspect quality into the product; it is already there.
         // I am not a product of my circumstances. I am a product of my decisions.`,
         image: bannerImage1
-        }
+    },
+    {
+        image: bannerImage1
+    },
+    {
+        image: bannerImage1
+    }
 ]
 
 const Banner:FC = () => {
@@ -43,25 +51,31 @@ const Banner:FC = () => {
                                               <h1 className="lg:text-[57px] md:text-[45px] text-[20px] font-normal md:leading-[53px] leading-[25px] mb-[8px] text-[#0AAEB9] capitalize slider__maintitle">
                                                     <span className='text-[#000]'>Shop</span> Computer & experience
                                               </h1>
-                                              <p className='mb-2'>You cannot inspect quality into the product; it is already there. <br />
-                                                I am not a product of my circumstances. I am a product of my decisions.</p>
+                                              <p className='mb-2 capitalize text-[15px]'>You cannot inspect quality into the product; it is already <br /> there. <br />
+                                                I am not a product of my circumstances. I am a product of <br /> my decisions.</p>
                                                 <a
-                                                    className="md:w-[168px] w-[100px] cursor-pointer tracking-widest flex items-center justify-center bg-[#14B1F0] text-[#fff] hover:bg-dark lg:text-[16px] text-[14px] font-bold lg:p-[12px] md:p-[8px] p-[6px]"
+                                                    className="md:w-[168px] w-[100px] cursor-pointer tracking-widest flex items-center justify-center bg-[#14B1F0] text-[#fff] hover:bg-dark lg:text-[16px] text-[14px] font-bold lg:p-[12px] md:p-[8px] p-[6px] rounded-[5px]"
                                                 //   onClick={() => }
                                                 >
                                                     View More
                                                 </a>
                                           </div>
                                       </div>
-                                      <div className='nextimg_wrapper'>
-                                          <Image
-                                              priority
-                                              className="next_img"
-                                              src={banner.image}
-                                              alt={banner.title}
-                                              title={banner.title}
-                                              layout="fill"
-                                          />
+                                      <div className="relative">
+                                        <div className='nextimg_wrapper relative'>
+                                            <Image
+                                                priority
+                                                className="next_img"
+                                                src={banner.image}
+                                                alt={banner.title}
+                                                title={banner.title}
+                                                layout="fill"
+                                            />
+                                        </div>
+                                        <div className='absolute top-[30px] right-[138px] flex items-center justify-center w-[165px] h-[159px] rounded-[83px] text-[47px] font-normal text-[#fff] leading-[58px] text-center hero_bnr_sticker'>
+                                            40% <br />
+                                            Off
+                                          </div>
                                       </div>
                                   </div>
                               </SwiperSlide>
