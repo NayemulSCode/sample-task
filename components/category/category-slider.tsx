a2cfe56a336cafebbead6eca5cc484cdad1231ab
hammadcse <hammadrahman162@gmail.com>
2023-05-30 01:21:52 +0600

    category design done

--- components/category/category-slider.tsx
+++ components/category/category-slider.tsx
@@ -4,6 +4,9 @@ import React, { useRef } from 'react'
 import { Swiper, SwiperSlide, Navigation } from '../ui/slider'
 import { ArrowPrevIcon } from '../icons/arrow-prev'
 import { ArrowNextIcon } from '../icons/arrow-next'
+import Image from 'next/image'
+import cat1 from '../../public/image/c1.png'
+import Link from 'next/link'
 
 const CategorySlider = () => {
     const {data: categories, isLoading, isError} = useGetCategoriesQuery();
@@ -31,7 +34,7 @@ const CategorySlider = () => {
         },
 
         1280: {
-            slidesPerView: 7,
+            slidesPerView: 4,
         },
     };
     if (isLoading) {
@@ -43,8 +46,8 @@ const CategorySlider = () => {
     }
   return (
       <>
-          <div className="m-auto lg:space-s-8 w-full md:py-10 py-3 px-4 md:px-6 max-w-[1640px] bg-[#F8F8F8] twxl_px_none lg:px-8 md:mb-0 mb-[20px]">
-              <div className="relative !ml-0">
+          <div className="m-auto lg:space-s-8 w-full md:py-10 py-3 md:mb-10 mb-3 px-4 md:px-6 max-w-[1640px] twxl_px_none lg:px-8 category_section_wrap">
+              <div className="relative !ml-0 inner">
                   <Swiper
                       modules={[Navigation]}
                       navigation={{
@@ -52,8 +55,8 @@ const CategorySlider = () => {
                           nextEl: nextRef.current!, // Assert non-null
                       }}
                       breakpoints={breakpoints}
-                      slidesPerView={3}
-                      spaceBetween={5}
+                      slidesPerView={4}
+                      spaceBetween={15}
 
                       autoplay={{
                           "delay": 1800,
@@ -63,30 +66,39 @@ const CategorySlider = () => {
                       {
                           categories && categories.map((category: any, index: number) => (
                               <SwiperSlide className='text-center' key={index}>
-                                <h4>{category}</h4>
-                                  {/* <Image
-                                      src={}
-                                      alt={}
-                                      height={60}
-                                      width={100}
-                                  /> */}
+                                <Link href="#">
+                                    <div className='relative border-[2px] border-[#fff]'>
+                                        <div className='cat_img_wrap'>
+                                            <Image
+                                                src={cat1}
+                                                alt="img"
+                                                height={200}
+                                                className="w-full"
+                                            />
+                                        </div>
+                                        <div className="absolute category_label flex items-center justify-between left-[-9px] bottom-[15px] px-[15px]">
+                                            <h4 className='text-[25px] font-normal leading-[53px] mr-[20px] text-[#000] capitalize'>{category}</h4>
+                                            <span className='text-[#14B1F0] text-[21px] leading-[53px]'>Shop</span>
+                                        </div>
+                                    </div>
+                                </Link>
                               </SwiperSlide>
                           ))
                       }
                   </Swiper>
                   <div
                       ref={prevRef}
-                      className="banner-slider-prev group_nav_prev hidden md:flex items-center justify-center text-heading bg-light outline-none absolute top-[31px] -mt-4 z-10 cursor-pointer !left-[0px] -start-[30px] lg:-start-4 focus:outline-none transition-all duration-200 hover:bg-black hover:text-light hover:border-black"
+                      className="banner-slider-prev group_nav_prev hidden md:flex items-center justify-center text-heading bg-light outline-none absolute top-[50%] -mt-4 z-10 cursor-pointer -start-[30px] lg:left-[-32px] focus:outline-none transition-all duration-200 text-[#000] hover:text-[#14B1F0]"
                   >
                       <span className="sr-only">{('text-previous')}</span>
-                      <ArrowPrevIcon />
+                      <ArrowPrevIcon className="w-[45px] text-[#000] hover:text-[#14B1F0]" />
                   </div>
                   <div
                       ref={nextRef}
-                      className="banner-slider-next group_nav_next hidden md:flex items-center justify-center text-heading bg-light outline-none absolute top-[31px] -mt-4 z-10 cursor-pointer !right-[0px] -end-[30px] lg:-end-4 focus:outline-none transition-all duration-200 hover:bg-black hover:text-light hover:border-black"
+                      className="banner-slider-next group_nav_next hidden md:flex items-center justify-center text-heading bg-light outline-none absolute top-[50%] -mt-4 z-10 cursor-pointer -end-[30px] lg:right-[-32px] focus:outline-none transition-all duration-200 text-[#000] hover:text-[#14B1F0]"
                   >
                       <span className="sr-only">{('text-next')}</span>
-                      <ArrowNextIcon />
+                      <ArrowNextIcon className="w-[45px] text-[#000] hover:text-[#14B1F0]" />
                   </div>
             </div>
         </div>
