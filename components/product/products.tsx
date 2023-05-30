c36f832f2e4905897426194eaa024cc0d2e8b2df
NayemulSCode <nayemulhasan2016@gmail.com>
2023-05-30 17:34:28 +0600

    product section finishing

--- components/product/products.tsx
+++ components/product/products.tsx
@@ -56,112 +56,83 @@ const Products:FC = () => {
         return <div>Error occurred while fetching products</div>
     }
   return (
-      <div className="relative m-auto lg:space-s-8 w-full px-2 twxl_px_none lg:px-7 max-w-[1640px] md:px-5 md:mb-[30px] mb-[20px]">
-          <div className="pb-[10x] sm:mt-[10px] mt-[6px] md:block hidden relative border-border-200 border-opacity-70">
-              <div className="flex flex-col items-center pb-[15px]">
-                  <h2 className="text-[#333] uppercase md:text-[22px] text-[20px] font_sprn_semibold mb-[0px] text-end">
-                      New Arrivals
-                  </h2>
-              </div>
-          </div>
-          <div className="relative !ml-0">
-              <Swiper
-                  modules={[Navigation]}
-                  navigation={{
-                      prevEl: prevRef.current!, // Assert non-null
-                      nextEl: nextRef.current!, // Assert non-null
-                  }}
-                  breakpoints={breakpoints}
-                  slidesPerView={6}
-              >
-                  <div className="">
-                      {products &&
-                          products.map((product: Product, index: number) => (
-                            <SwiperSlide key={index}>
-                                  {/* <div className="relative group promo_single_item my-[10px] offer_single_item flsh_wd_item md:h-[446px] text-center cursor-pointer rounded-bl-[5px] rounded-br-[5px] bg-[#ffffff] rounded-[5px]">
-                                      <div className="md:p-[15px] p-[8px] md:pt-[15px] pt-[40px] relative text-[#000000] bg-[#ffffff] rounded-bl-[5px] rounded-br-[5px]"> 
-                                          <p>{product.category}</p>
-                                          <Link href={`/products/${product?.id}`}>
-                                              {product?.title}
-                                          </Link>
-
-                                      </div>
-                                      <div className="promo_card flsh_wd_img bg-[#ffffff] md:h-[315px] flex items-center justify-center rounded-tl-[5px] rounded-tr-[5px]">
-                                          <Link href={`/products/${product?.id}`}>
-                                              <div className="nextimg_wrapper">
-                                                  <Image
-                                                    src={product.image}
-                                                    alt={product.title}
-                                                    className="next_img m-auto"
-                                                    height={270}
-                                                    width={280}
-                                                  />
-                                              </div>
-                                          </Link>
-                                      </div>
-                                      <div className="md:p-[15px] p-[8px] md:pt-[15px] pt-[40px] relative bg-[#ffffff] rounded-bl-[5px] rounded-br-[5px]">
-                                          <div className="md:flex justify-center">
-                                              <span className="text-[#000000] md:text-[16px] text-[15px] font-bold line-clamp-1 md:mr-[10px]">
-                                                  BDT {product?.price}
-                                              </span>
-                                              <span className="text-[#98928e] md:text-[13px] text-[11px] font-medium line-clamp-1">
-                                                    <del>BDT 1,000</del>
-                                                </span>
-                                          </div>
-                                      </div>
-                                  </div> */}
-
-                                  <div className="promo_single_item offer_single_item text-center bg-light cursor-pointer p-[10px]">
-                                        <p className='text-[#000] text-[12px] font-normal leading-[15px] mb-[5px] line-clamp-1'>{product.category}</p>
-                                        <Link href={`/products/${product?.id}`} className="font-normal text-[15px] leading-[18px] text-[#034E53] line-clamp-1 ">
-                                            {product?.title}
-                                        </Link>
-                                    <div className="promo_card">
-                                        <div className="promo_card_img">
-                                            <Image
-                                                src={product.image}
-                                                alt={product.title}
-                                                title={product.title}
-                                                // layout={`fill`}
-                                                height={200}
-                                                width={300}
-                                            />
-                                        </div>
-                                    </div>
-                                    <div className="relative">
-                                        <div className="md:flex items-center justify-center mb-[12px]">
-                                            <span className="text-[#697475] md:text-[13px] leading-[16px] text-[11px] line-clamp-1 md:mr-[10px] font-normal">
-                                                <del>RS 1,000</del>
-                                            </span>
-                                            <span className="text-[#0AAEB9] md:text-[15px] leading-[18px] text-[13px] font-normal line-clamp-1">
-                                                RS {product?.price}
-                                            </span>
-                                        </div>
-                                        <button className='bg-[#15ADB7] hover:bg-[#03484D] text-[#fff] border-0 outline-0 px-[5px] py-[8px] w-full'>Add to cart</button>
-                                    </div>
-                                  </div>
-
-                              </SwiperSlide>
-                          ))}
-                  </div>
-              </Swiper>
-              <div
-                  ref={prevRef}
-                  className="banner-slider-prev promo_slide_prev mt-[-7px] flex items-center justify-center text-[#333]  absolute z-10 cursor-pointer transition-all duration-200 bg-[#943590ad] hover:bg-darkBlue hover:text-light hover:border-[#943590ad]"
-              >
-                  <span className="sr-only">{'text-previous'}</span>
-                  <ArrowPrevIcon width={25} height={25}  />
-              </div>
-              <div
-                  ref={nextRef}
-                  className="banner-slider-prev promo_slide_prev mt-[-7px] flex items-center justify-center text-[#333]  absolute z-10 cursor-pointer transition-all duration-200 bg-[#943590ad] hover:bg-darkBlue hover:text-light hover:border-[#943590ad]"
-              >
-                  <span className="sr-only">{'text-previous'}</span>
-                  <ArrowNextIcon width={25} height={25} />
-              </div>
-          </div>
+    <div className="relative m-auto lg:space-s-8 w-full px-2 twxl_px_none lg:px-7 max-w-[1640px] md:px-5 md:mb-[30px] mb-[20px]">
+      <div className="pb-[10x] sm:mt-[10px] mt-[6px] md:block relative border-border-200 border-opacity-70">
+        <div className="flex flex-col pb-[15px]">
+          <h2 className="text-[#333] uppercase md:text-[22px] text-[20px] font_sprn_semibold mb-[0px] text-start">
+            <span className="text-[#15ADB7]">New</span> Arrivals
+          </h2>
+        </div>
       </div>
-  )
+      <div className="relative !ml-0">
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            prevEl: prevRef.current!, // Assert non-null
+            nextEl: nextRef.current!, // Assert non-null
+          }}
+          breakpoints={breakpoints}
+          slidesPerView={6}
+        >
+          <div className="">
+            {products &&
+              products.map((product: Product, index: number) => (
+                <SwiperSlide key={index}>
+                  <div className="product_single_item offer_single_item text-center bg-light cursor-pointer p-[10px]">
+                    <p className="text-[#000] text-[12px] font-normal leading-[15px] mb-[5px] line-clamp-1">
+                      {product.category}
+                    </p>
+                    <Link
+                      href={`/products/${product?.id}`}
+                      className="font-normal text-[15px] leading-[18px] text-[#034E53] line-clamp-1 "
+                    >
+                      {product?.title}
+                    </Link>
+                    <div className="product_card">
+                      <div className="product_card_img">
+                        <Image
+                          src={product.image}
+                          alt={product.title}
+                          title={product.title}
+                          // layout={`fill`}
+                          height={200}
+                          width={300}
+                        />
+                      </div>
+                    </div>
+                    <div className="relative">
+                      <div className="md:flex items-center justify-center mb-[12px]">
+                        <span className="text-[#697475] md:text-[13px] leading-[16px] text-[11px] line-clamp-1 md:mr-[10px] font-normal">
+                          <del>BDT 1,000</del>
+                        </span>
+                        <span className="text-[#0AAEB9] md:text-[15px] leading-[18px] text-[13px] font-normal line-clamp-1">
+                          BDT {product?.price}
+                        </span>
+                      </div>
+                      <button className="bg-[#15ADB7] hover:bg-[#03484D] text-[#fff] border-0 outline-0 px-[5px] py-[8px] w-full">
+                        Add to cart
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+          </div>
+        </Swiper>
+        <div
+          ref={prevRef}
+          className="product_slide_prev mt-[-7px] flex items-center justify-center text-[#333]  absolute z-10 cursor-pointer transition-all duration-200 bg-[#15ADB7] hover:bg-darkBlue hover:text-light"
+        >
+          <ArrowPrevIcon width={25} height={25} />
+        </div>
+        <div
+          ref={nextRef}
+          className="product_slide_next mt-[-7px] flex items-center justify-center text-[#333]  absolute z-10 cursor-pointer transition-all duration-200 bg-[#15ADB7] hover:bg-darkBlue hover:text-light"
+        >
+          <ArrowNextIcon width={25} height={25} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Products
